a6aaba4ac933e50681eab3333153104bb42630f7
Brian <ashe1990@hotmail.com>
2023-02-28 22:59:07 -0600

    SergUpdate3

--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
 const express = require("express");
-const { dbConnection } = require("./database/config");
-
 const app = express();
 app.use(express.json());
-
-dbConnection();
 
 app.use(require("./routes/index"));
 
